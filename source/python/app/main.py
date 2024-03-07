67db2784f541657281ebaa86ccd279d593a70888
김병준 <120731337+aquaheyday@users.noreply.github.com>
2024-03-07 13:29:27 +0900

    FEAT 택배 조회 기본 api 생성

--- source/python/app/main.py
+++ source/python/app/main.py
@@ -3,7 +3,7 @@ import pymysql
 
 app = FastAPI(docs_url='/api/docs', openapi_url='/api/openapi.json')
 
-@app.get("/")
+""" @app.get("/")
 async def first_get():
     example = session.query(Test).all()
     return example
@@ -15,7 +15,7 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q} """
 
 # 데이터베이스 연결 설정
 def get_db_connection():
@@ -27,7 +27,7 @@ def get_db_connection():
         cursorclass=pymysql.cursors.DictCursor
     )
 
-@app.get("/api/tests/")
+""" @app.get("/api/tests/")
 def read_items():
     connection = get_db_connection()
     try:
@@ -41,4 +41,76 @@ def read_items():
             return result
     finally:
         # 데이터베이스 연결 종료
+        connection.close() """
+
+@app.get("/api/companyList")
+def read_item():
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            sql1 = "SELECT name FROM deliveryCompany WHERE type = 'kr'"
+            cursor.execute(sql1)
+
+            resultKr = cursor.fetchall()
+
+            sql2 = "SELECT name FROM deliveryCompany WHERE type = 'en'"
+            cursor.execute(sql2)
+
+            resultEn = cursor.fetchall()
+
+            return {"kr": resultKr, "en": resultEn}
+    finally:
+        connection.close()
+
+
+@app.get("/api/ratePlan")
+def read_item():
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT type, monthlyPlan, selectLimit, daySameSelectLimit, trackingPageYn FROM ratePlan"
+            cursor.execute(sql)
+
+            result = cursor.fetchall()
+
+            return result
+    finally:
+        connection.close()
+
+@app.get("/api/description")
+def read_item():
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM inquiry"
+            cursor.execute(sql)
+
+            result = cursor.fetchall()
+
+        return result
+    finally:
+        connection.close()
+
+
+class Item(BaseModel):
+    name: str
+    number: str
+    email: str = None
+    type: str = None
+    description: str
+    agreeYn: str = 'N'
+
+@app.post("/api/description")
+async def create_item(item: Item):
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO inquiry (name, number, email, type, description, agreeYn) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (item.name, item.number, item.email, item.type, item.description, item.agreeYn))
+
+            connection.commit()
+            id = cursor.lastrowid
+
+            return id
+    finally:
         connection.close()
